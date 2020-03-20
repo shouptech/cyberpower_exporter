1c1c9e087491e94858bf8c26030c86a27a5f1a80
Mike Shoup <mike@shoup.io>
2020-03-19 19:58:33 -0600

    Add more collectors

--- src/cyberpower_exporter/command.py
+++ src/cyberpower_exporter/command.py
@@ -33,11 +33,33 @@ def register_prometheus_collectors():
         "cyberpower", "Information about UPS"
     )
     collectors["utility_volt"] = prometheus_client.Gauge(
-        "utility_volt", "Voltage from the utility"
+        "cyberpower_utility_volt", "Voltage from the utility"
     )
     collectors["output_volt"] = prometheus_client.Gauge(
-        "output_volt", "Voltage output"
+        "cyberpower_output_volt", "Voltage output"
     )
+    collectors["diagnostic_result"] = prometheus_client.Gauge(
+        "cyberpower_diagnostic_result", "Result of last diagnostic"
+    )
+    collectors["battery_remainingtime"] = prometheus_client.Gauge(
+        "cyberpower_battery_remainingtime", "Seconds of battery time remaining"
+    )
+    collectors["battery_charging"] = prometheus_client.Gauge(
+        "cyberpower_battery_charging", "Is battery charging"
+    )
+    collectors["battery_discharging"] = prometheus_client.Gauge(
+        "cyberpower_battery_discharging", "Is battery discharging"
+    )
+    collectors["ac_present"] = prometheus_client.Gauge(
+        "cyberpower_ac_present", "Is AC power present"
+    )
+    collectors["load"] = prometheus_client.Gauge(
+        "cyberpower_load", "Load percentage"
+    )
+    collectors["battery_capacity"] = prometheus_client.Gauge(
+        "cyberpower_battery_capacity", "Percentage of battery remaining"
+    )
+    collectors[""]
     return collectors
 
 
@@ -55,9 +77,43 @@ def set_prometheus_values(collectors):
     )
     collectors["utility_volt"].set(float(data["utility_volt"]) / 1000)
     collectors["output_volt"].set(float(data["output_volt"]) / 1000)
+    collectors["diagnostic_result"].set(int(data["diagnostic_result"]))
+    collectors["battery_remainingtime"].set(int(data["battery_remainingtime"]))
+    collectors["battery_charging"].set(
+        1 if data["battery_charging"] == "yes" else 0
+    )
+    collectors["battery_discharging"].set(
+        1 if data["battery_discharging"] == "yes" else 0
+    )
+    collectors["ac_present"].set(1 if data["ac_present"] == "yes" else 0)
+    collectors["load"].set(float(data["load"]) / 100000)
+    collectors["battery_capacity"].set(float(data["battery_capacity"]) / 100)
 
 
 def get_data():
+    # Sample output:
+    # state=0
+    # model_name=PR750LCD
+    # firmware_num=PQ6BN2001641
+    # battery_volt=24000
+    # input_rating_volt=120000
+    # output_rating_watt=525000
+    # avr_supported=yes
+    # online_type=no
+    # diagnostic_result=1
+    # diagnostic_date=2020/03/19 15:05:30
+    # power_event_result=0
+    # battery_remainingtime=621
+    # battery_charging=no
+    # battery_discharging=no
+    # ac_present=yes
+    # boost=no
+    # buck=no
+    # utility_volt=122000
+    # output_volt=122000
+    # load=50000
+    # battery_capacity=100
+
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     s.connect("/var/pwrstatd.ipc")
     s.sendall(b"STATUS\n\n")
